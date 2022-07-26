e2ebef04de40b08c907cba5a2db76c054b2916c7
Meghsham15 <88832001+Meghsham15@users.noreply.github.com>
2022-07-27 00:15:12 +0530

    Added Environment variables

---
+++ app.js
@@ -0,0 +1,75 @@
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const ejs = require('ejs');
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema =  new mongoose.Schema({
+    email : String,
+    password : String
+});
+
+// const secret = process.env.SECRET;
+userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+
+const User = new mongoose.model("User",userSchema);
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/register",function(req,res){
+    res.render("register");
+});
+
+
+app.post("/register",function(req,res){
+    const newUser = new User({
+        email : req.body.username,
+        password : req.body.password
+    });
+    newUser.save(function(err){
+        if(err){
+            res.send(err);
+        }else{
+            res.render("secrets");
+        };
+    });
+})
+
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.post("/login",function(req,res){
+    const userName = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email:userName},function(err,foundUser){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets");
+                }
+            };
+        };
+    });
+});
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000,function(){
+    console.log("Server is running at server 3000");
+  });
